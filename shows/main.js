437fd27a0a938fa0ae33df66d10cacbf5807935e
rakoo <matthieu.rakotojaona@gmail.com>
2012-05-11 02:06:20 +0200

    mustache the path to attachment

--- shows/main.js
+++ shows/main.js
@@ -1,13 +1,20 @@
 function (doc, req) {
-  // !json templates.page
+    // !json templates.page
 
-  var mustache = require("vendor/couchapp/lib/mustache");
-  return mustache.to_html(this.templates.moge, data);
-  return Mustache.template(template.page,{
-    version: 1.0,
-    errormessage: "hey",
-    cipherdata: "test"
-  })
+    var mustache = require("vendor/couchapp/lib/mustache");
 
+    var path;
+    if (typeof(req.path[5]) === 'undefined') {
+        path = "../"
+    } else {
+        path = "../../"
+    }
 
+    data = {
+        attachment_path: path,
+        version: 1.0,
+        errormessage: "hey",
+        cipherdata: typeof(req.path[5])
+    }
+    return mustache.to_html(this.templates.page, data);
 }
