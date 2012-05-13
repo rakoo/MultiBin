25658810746a1e0e595cee9937dcdb9eee666eda
rakoo <matthieu.rakotojaona@gmail.com>
2012-05-13 16:57:41 +0200

    Add basic well-formedness verification before storing a doc
    A new document must have a "type" field that contains either "paste" or
    "comment", and must have a "data" field that contains a String.

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,8 +1,20 @@
 function(newDoc, oldDoc, userCtx) {
-  
   if (oldDoc){
     if (userCtx.roles.indexOf("_admin") == -1) {
       throw({forbidden: "Only database admins are allowed to update pastes/comments"});
+    }
+  } else {
+    // verify well-formedness
+
+    var malformed = false;
+    if (typeof(newDoc.data) != "string") {
+      malformed = true;
+    } else if (["paste", "comment"].indexOf(newDoc.type) == -1) {
+      malformed = true;
+    }
+
+    if (malformed == true) {
+      throw({forbidden: "The document is malformed. Not accepting this."});
     }
   }
 } 
